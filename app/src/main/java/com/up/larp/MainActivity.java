93b847f1ac2ab08420413a2e95703fd6c1d3cbb2
Dabu <rafal.adamek@hotmail.com>
2020-04-01 17:50:13 +0200

    Base project setup

---
+++ app/src/main/java/com/up/larp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.up.larp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
