5e926b71774dbd8392403a817332ba20f6d41c89
Dabu <rafal.adamek@hotmail.com>
2020-04-13 16:42:51 +0200

    Add camera floating button

--- app/src/main/java/com/up/larp/MainActivity.java
+++ app/src/main/java/com/up/larp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.up.larp;
 
+import android.view.View;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCamera();
+            }
+        });
+    }
+
+    private void showCamera() {
+        Toast.makeText(MainActivity.this, "Test", Toast.LENGTH_SHORT).show();
     }
 }
