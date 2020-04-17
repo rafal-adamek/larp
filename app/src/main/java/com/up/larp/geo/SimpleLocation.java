9ecde8ac4e3bb8184337e2396bacc0b7997f8a48
Dabu <rafal.adamek@hotmail.com>
2020-04-17 14:11:55 +0200

    Add observing user location

---
+++ app/src/main/java/com/up/larp/geo/SimpleLocation.java
@@ -0,0 +1,27 @@
+package com.up.larp.geo;
+
+public class SimpleLocation {
+    public double lat;
+    public double lon;
+
+    public SimpleLocation(double lat, double lon) {
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+}
